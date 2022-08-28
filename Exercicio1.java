38c2dde2fc4fa84157918fc4ae57f1f7312dcb75
alulucas <aluiziolucas7@gmail.com>
2022-08-28 16:27:27 -0300

    first commit

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Exercicio1{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String endereco in.nextLine();
+        
+        
+
+    }
+
+}
